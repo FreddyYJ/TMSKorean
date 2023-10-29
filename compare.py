841e1961c4a4b177078c75d3dd6b22ae646dd962
FreddyYJ <kyj1411@unist.ac.kr>
2023-10-29 20:56:18 +0900

    Update original translation

--- compare.py
+++ compare.py
@@ -1,7 +1,7 @@
-korean=open('original/Custom.csv','r')
+korean=open('Improved-Korean.csv','r')
 korean_keys=korean.readlines()
 
-original=open('original/Current.csv','r')
+original=open('Custom.csv','r')
 original_keys=original.readlines()
 
 for key in korean_keys:
